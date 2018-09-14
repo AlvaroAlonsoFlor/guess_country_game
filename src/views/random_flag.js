7714efabd89f371216be94d9a70325c86b3bc287
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-14 16:57:24 +0100

    Add a section for flag, create and append random flag

--- src/views/random_flag.js
+++ src/views/random_flag.js
@@ -1,14 +1,29 @@
 const PubSub = require('../helpers/pub_sub.js');
+const createAndAppend = require('../helpers/create_append.js');
 const Request = require('../helpers/create_append.js');
 
 const RandomFlag = function() {
-
+  this.container = document.querySelector('#flag-container')
 }
 
 RandomFlag.prototype.bindEvents = function () {
   PubSub.subscribe('Countries:all', (e) => {
-    console.log(e.detail);
+    const countryFlag = this.getFlag(e.detail);
+    const element = createAndAppend('img', 'flag', '', this.container);
+    element.src = countryFlag;
+
+
+    console.log(countryFlag);
+
   });
 };
+
+RandomFlag.prototype.getFlag = function (countries) {
+  return countries[this.randomInt(countries.length)].flag;
+};
+
+RandomFlag.prototype.randomInt = function(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 module.exports = RandomFlag;
