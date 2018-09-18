ef1235f74cb43532af11d731aa92b219e37f5733
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-09-18 19:14:57 +0100

    Add randomInt and getRandomCountry to the countries model

--- src/models/countries.js
+++ src/models/countries.js
@@ -3,16 +3,18 @@ const Request = require('../helpers/request.js');
 
 const Countries = function() {
   this.countries = []
+  this.country = []
+  this.request = new Request('https://restcountries.eu/rest/v2/all');
 };
 
 Countries.prototype.bindEvents = function () {
   this.getData();
+  this.getRandomCountry();
 
 };
 
 Countries.prototype.getData = function () {
-  const request = new Request('https://restcountries.eu/rest/v2/all');
-  request.get()
+  this.request.get()
     .then((data) => {
       this.countries = data;
       PubSub.publish('Countries:all', this.countries);
@@ -22,5 +24,17 @@ Countries.prototype.getData = function () {
       console.error(message);
     });
 };
+
+GuessCountry.prototype.getRandomCountry = function (countries) {
+  this.country = countries[this.randomInt(countries.length)];
+  PubSub.publish('Countries:random', this.country);
+};
+
+Countries.prototype.randomInt = function(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+
 
 module.exports = Countries;
